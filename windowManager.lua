aa8ecb1fdbd67702a2c2516c821e0a3d39e6569c
orthoplex64 <orthoplex64@gmail.com>
2016-04-22 15:42:43 -0500

    Work on gpuWrapper

--- windowManager.lua
+++ windowManager.lua
@@ -3,11 +3,11 @@
 local component = require("component")
 local wm = {}
 
-local function rectIntersection(x1, y1, w1, h1, x2, y2, w2, h2)
+function wm.rectIntersection(x1, y1, w1, h1, x2, y2, w2, h2)
   local xmin1, ymin1, xmax1, ymax1, xmin2, ymin2, xmax2, ymax2
     = x1, y1, x1 + w1, y1 + h1, x2, y2, x2 + w2, y2 + h2
   -- first test intersection
-  if xmin2 > xmax1 or xmax2 < xmin1 or ymin2 > ymax1 or ymax2 < ymin1 then return nil end
+  if xmin2 > xmax1 or xmax2 < xmin1 or ymin2 > ymax1 or ymax2 < ymin1 then return end
   -- consider rect1 static; squish rect2 to fit within rect1
   if xmin2 < xmin1 then xmin2 = xmin1 end
   if xmax2 > xmax1 then xmax2 = xmax1 end
@@ -16,10 +16,34 @@ local function rectIntersection(x1, y1, w1, h1, x2, y2, w2, h2)
   return xmin2, ymin2, xmax2 - xmin2 + 1, ymax2 - ymin2 + 1
 end
 
-local function gpuWrapper(gpu, clipX, clipY, clipW, clipH)
+function wm.gpuWrapper(gpu, clipX, clipY, clipW, clipH)
+  if not clipX then
+    clipX, clipY = 0, 0
+    clipW, clipH = gpu.getResolution()
+  end
   local res = {}
   res.__index = gpu
   local prevState = {palette = {}}
+  function res.wrapperGetClip()
+    return clipX, clipY, clipW, clipH
+  end
+  function res.wrapperSetClip(x, y, width, height)
+    clipX, clipY, clipW, clipH = x, y, width, height
+  end
+  function res.wrapperRestorePrevState()
+    if prevState.colorDepth then
+      gpu.setDepth(prevState.colorDepth)
+    end
+    if prevState.bgColor then
+      gpu.setBackground(prevState.bgColor, prevState.bgColorIsPaletteIndex)
+    end
+    if prevState.fgColor then
+      gpu.setForeground(prevState.fgColor, prevState.fgColorIsPaletteIndex)
+    end
+    for k, v in pairs(prevState.palette) do
+      gpu.setPaletteColor(k, v)
+    end
+  end
   function res.setBackground(...)
     if not prevState.bgColor then
       prevState.bgColor, prevState.bgColorIsPaletteIndex = gpu.getBackground()
@@ -57,8 +81,22 @@ local function gpuWrapper(gpu, clipX, clipY, clipW, clipH)
     return gpu.set(clipX + (x - 1), clipY + (y - 1), value, vertical)
   end
   function res.copy(x, y, width, height, tx, ty)
-    asdf
+    if x < 1 or x > clipW or y < 1 or y > clipH
+      or tx < 1 or tx > clipW or ty < 1 or ty > clipH
+      then return end
+    width = math.min(width, clipW - (x - 1), clipW - (tx - 1))
+    height = math.min(height, clipH - (y - 1), clipH - (ty - 1))
     return gpu.copy(x, y, width, height, tx, ty)
+  end
+  function res.fill(x, y, width, height, char)
+    if x < 1 or x > clipW or y < 1 or y > clipH then return end
+    if width > clipW - (x - 1) then
+      width = clipW - (x - 1)
+    end
+    if height > clipH - (y - 1) then
+      height = clipH - (y - 1)
+    end
+    return gpu.fill(x, y, width, height, char)
   end
   return res
 end
